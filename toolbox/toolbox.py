36f8f160fdbe082be7791673fd842ae27da4a0ef
Sheldon McGrandle <developer@8cylinder.com>
2024-04-26 16:55:05 -0700

    Rough in Transfer._rsync

--- toolbox/toolbox.py
+++ toolbox/toolbox.py
@@ -5,6 +5,8 @@ import click
 from toolbox.config import Action
 from toolbox.config import project
 from toolbox.output import l
+from toolbox.transfer import Transfer
+from pathlib import Path
 
 __version__ = "2.0.0"
 
@@ -32,14 +34,12 @@ CONTEXT_SETTINGS = {
 }
 
 
+# fmt: off
 @click.group(context_settings=CONTEXT_SETTINGS, cls=NaturalOrderGroup)
-@click.option(
-    "-s",
-    "--suppress-commands",
-    is_flag=True,
-    help="Don't display the bash commands used.",
-)
+@click.option("-s", "--suppress-commands", is_flag=True,
+    help="Don't display the bash commands used.")
 @click.version_option()
+# fmt: on
 def toolbox(suppress_commands):
     """🛠  Tools to manage projects.
 
@@ -61,23 +61,21 @@ def toolbox(suppress_commands):
 
 
 # --------------------------------- DB ---------------------------------
+# fmt: off
 @toolbox.command("db", context_settings=CONTEXT_SETTINGS)
 @click.argument("action", type=click.Choice([i.value for i in Action]))
 # @click.argument('server', type=click.Choice(get_server_choices()), shell_complete=get_servers)
 @click.argument("server", type=click.STRING, shell_complete=get_servers)
 @click.argument("sql-gz", type=click.Path(exists=True), required=False)
-@click.option(
-    "--tag",
-    "-t",
-    type=click.STRING,
-    help="Add a tag to the generated filename when pulling.",
-)
-@click.option(
-    "-q", "--quiet", count=True, help="-q: Output the filename, -qq: output nothing."
-)
-@click.option("--real", "-r", is_flag=True, help="Run the command for real.")
+@click.option("--tag", "-t", type=click.STRING,
+    help="Add a tag to the generated filename when pulling.")
+@click.option("-q", "--quiet", count=True,
+    help="-q: Output only the filename, -qq: output nothing.")
+@click.option("--real", "-r", is_flag=True,
+    help="Run the command for real.")
 @click.pass_context
-def database(ctx, action, sql_gz, server, quiet, real, tag):
+# fmt: on
+def database(action, sql_gz, server, quiet, real, tag):
     """Overwrite a db with a gzipped sql file.
 
     \b
@@ -119,3 +117,46 @@ def database(ctx, action, sql_gz, server, quiet, real, tag):
     #     if not sql_gz:
     #         ui.error('When action is "put", SQL-GZ is required.')
     #     db.put(server, sql_gz)
+
+
+# ------------------------------- Files -------------------------------
+# fmt: off
+@toolbox.command('file', context_settings=CONTEXT_SETTINGS)
+@click.argument('action', type=click.Choice([str(i.value) for i in Action]))
+@click.argument('server', shell_complete=get_servers)
+@click.argument('filename', type=click.Path(), required=False)
+@click.option('--real', '-r', is_flag=True)
+@click.option("-q", "--quiet", count=True,
+    help="-q: Output only the filename, -qq: output nothing.")
+@click.option('--extra-flags',
+    help='extra flags to pass to rsync.')
+# fmt: on
+def files(action, filename, server, real, quiet, extra_flags):
+    """Send files to and fro.
+    Push or pull a single file or directory from a remote server.
+    \b
+    ACTION: pull or put
+    SERVER: server name, if not specified sink will use the default server.
+    FILENAME: file/dir to be transferred."""
+
+    if not project.in_project:
+        l.error("Not in a project.", exit=True)
+    else:
+        l.info(f"Project: {project.name}")
+
+    # if it's a put action and the local file does not exist, error out
+    if filename and action == Action.PUT.value and not os.path.exists(filename):
+        l.error(f"File '{filename}' does not exist.", exit=True)
+
+    if filename:
+        f = Path(os.path.abspath(filename))
+    else:
+        f = Path(project.root)
+
+    transfer = Transfer(real, server_name=server, quiet=quiet)
+    transfer.transfer(action, f, extra_flags)
+
+    # if action == Action.PULL.value:
+    #     xfer.pull(f, extra_flags)
+    # elif action == Action.PUT.value:
+    #     xfer.put(f, extra_flags)
---
+++ toolbox/transfer.py
@@ -0,0 +1,136 @@
+import os
+import sys
+from pathlib import Path
+from plumbum import local
+from plumbum.cmd import rsync
+from typing import List
+
+from toolbox.config import project
+from toolbox.config import Action
+from toolbox.output import l
+
+
+class Transfer:
+
+    def __init__(self, real: bool, server_name: str, quiet: object = False) -> None:
+        """Initialize the Transfer class.
+
+        :param real: true to actually do the transfer, else just print the command.
+        :param server_name: the name of the server to transfer to or from.
+        :param quiet:
+        """
+        self.server = project.get_server_by_name(server_name)
+        self.real = real
+        self.quiet = quiet
+
+    def transfer(
+        self, action: Action, filename: os.PathLike, extra_flags: List = None
+    ) -> None:
+        """Transfer a file to or from a remote server.
+
+        :param action: Action.PULL or Action.PUT
+        :param filename:  the local file name to transfer
+        :param extra_flags: additional flags to pass to rsync
+        """
+        remote = self._get_matching_remote(filename)
+        self._rsync(action, filename, remote, extra_flags)
+
+    # def put(self, filename: os.PathLike, extra_flags: array = None):
+    #     pass
+    #
+    # def pull(self, filename: os.PathLike, extra_flags: array = None):
+    #     remote = self.get_remote(self.server.root, filename)
+    #     self._rsync(filename, remote, Action.PULL, extra_flags)
+
+    def __XXXXlocations(self, filename):
+        p = self.config.project()
+        # s = self.config.server(server)
+        s = self.server
+        local = filename
+        remote = str(local)
+        # remove the local project root from the file
+        remote = remote.replace(str(p.root.absolute()), ".")
+        # add the server root
+        try:
+            remote = Path(s.root, remote)
+        except TypeError:
+            ui.error(f"Server has no root ({s.servername}).")
+        file_locations = {
+            "local": local,
+            "remote": remote,
+        }
+        return file_locations
+
+    def _get_matching_remote(self, filename: os.PathLike) -> os.PathLike:
+        """Get the remote path that matches the local path."""
+        remote = str(filename)
+        # remove the local project root from the file
+        remote = remote.replace(str(project.root.absolute()), ".")
+        # add the server root
+        try:
+            remote = Path(self.server.root, remote)
+        except TypeError:
+            l.error(f"Server has no root ({self.server.servername}).")
+        return remote
+
+    def _rsync(
+        self,
+        action: Action,
+        local_file: os.PathLike,
+        remote: os.PathLike,
+        extra_flags: List = None,
+    ) -> None:
+
+        args = []
+
+        if not self.real:
+            args += ["--dry-run"]
+
+        ssh = self.server.ssh[0]
+        if ssh.key:
+            args += ["--rsh", f'"ssh -i {ssh.key}"']
+
+        args += ["--links", "--compress", "--checksum", "--itemize-changes"]
+
+        if ssh.port:
+            # https://stackoverflow.com/a/4630407
+            args += ["-e", f'"-p {ssh.port}"']
+
+        # if transferring a dir, add the recursive flag, any excludes and
+        # end the dirs with trailing slashes.
+        if local_file.is_dir():
+            args += ["--recursive"]
+
+            project_excludes = [] if not project.exclude else project.exclude
+            server_excludes = [] if not self.server.exclude else self.server.exclude
+            if excludes := list(set(project_excludes + server_excludes)):
+                excludes = ",".join(f'"{i}"' for i in excludes)
+                args += ["--exclude", f"'{{{excludes}}}'"]
+
+            local_file = os.path.join(
+                local_file, ""
+            )  # append a slash to the end of the path
+            remote = os.path.join(remote, "")  # append a slash to the end of the path
+
+        if action == Action.PUT and (s.group or s.user):
+            # To have rsync change owner or group, the '--group' and
+            # '--owner' flags have to be used as well as '--chown'
+            # otherwise they will be ignored.
+            args += [
+                "--group" if self.server.group else "",
+                "--owner" if self.server.user else "",
+                "--chown",
+                f"{self.server.group}:{self.server.user}",
+            ]
+
+        if action == Action.PUT.value:
+            args += [local_file, f"{ssh.username}@{ssh.server}:{remote}"]
+        else:
+            args += [f"{ssh.username}@{ssh.server}:{remote}", local_file]
+
+        rsync_cmd = "rsync"
+        if custom_rsync_cmd := project.rsync_binary[sys.platform]:
+            rsync_cmd = custom_rsync_cmd
+        rsync = local[rsync_cmd]
+        rsync = rsync[args]
+        print(str(rsync))
